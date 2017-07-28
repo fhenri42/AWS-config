32be906b346882732bdff60562301c2496ac6377
Floren Henri <fhenri@student.42.fr>
2017-07-28 16:56:47 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+sudo apt-get install curl
+sudo apt-get install node
+npm install -g yarn
+git clone https://github.com/tj/n.git
+cd n/bin  && sudo n stable cd 
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+zsh
