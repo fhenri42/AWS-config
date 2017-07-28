e32f2615bcde5e2bbb784ee0ccb0b6716bc505b6
Floren Henri <fhenri@student.42.fr>
2017-07-28 17:03:20 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 cd ~/
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo apt-get install curl
 sudo apt-get install nodejs
 npm install -g yarn
 git clone https://github.com/tj/n.git
 cd n/bin  && sudo n stable cd 
-sudo apt-get install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 zsh
